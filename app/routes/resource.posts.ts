96589a42028cd11d74cfb2a7dc7f84be418e4c1f
Charlie Gleason <hi@charliegleason.com>
2024-05-30 11:46:57 +0100

    Noodled on posts

--- app/routes/resource.posts.ts
+++ app/routes/resource.posts.ts
@@ -1,8 +1,12 @@
 import { json } from '@remix-run/cloudflare'
 import { getPosts } from '~/.server/posts'
 
-export const loader = async () => {
-  const posts = await getPosts()
+export const loader = async ({ limit = 5 }: { limit: number }) => {
+  let posts = await getPosts()
+
+  if (limit >= 0) {
+    posts = posts.slice(0, limit)
+  }
 
   return json(posts, {
     status: 200,

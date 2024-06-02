4626039d81b9367308e92f0e122567af9d625f5e
Charlie Gleason <hi@charliegleason.com>
2024-06-02 11:16:46 +0100

    Updated images

--- app/components/diagram.tsx
+++ app/components/diagram.tsx
@@ -3,9 +3,14 @@ export default function Diagram({ src, alt }: { src: string; alt: string }) {
   const darkImage = `${baseImage.split('.').join('-dark.')}`
   console.log(baseImage, darkImage)
   return (
-    <picture className={`overflow-hidden rounded-lg inline-flex align-top`}>
-      <source media="(prefers-color-scheme: dark)" srcSet={darkImage} />
-      <img src={baseImage} alt={alt} />
-    </picture>
+    <div className="my-8">
+      <picture className={`overflow-hidden rounded w-full align-top my-0`}>
+        <source media="(prefers-color-scheme: dark)" srcSet={darkImage} />
+        <img src={baseImage} alt={alt} className=" w-full" />
+      </picture>
+      <figcaption className="text-center leading-relaxed text-balance flex-shrink px-8 mb-2">
+        {alt}
+      </figcaption>
+    </div>
   )
 }

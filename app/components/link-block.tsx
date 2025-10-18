03648fe5c7c6d34fa272a7eab313ffca11612e15
Charlie Gleason <hi@charliegleason.com>
2025-10-18 16:46:05 -0700

    Tidy link block

--- app/components/link-block.tsx
+++ app/components/link-block.tsx
@@ -16,27 +16,29 @@ export default function LinkBlock({
   return (
     <a
       href={href}
-      className="flex flex-col group hover:bg-gray-50 dark:hover:bg-gray-900 relative before:content-[''] before:absolute before:left-0 before:top-0 before:h-full before:w-[2px] before:bg-transparent hover:before:bg-indigo-500 px-4 py-3 gap-1"
+      className="@container flex flex-col group hover:bg-gray-50 dark:hover:bg-gray-900 relative before:content-[''] before:absolute before:left-0 before:top-0 before:h-full before:w-[2px] before:bg-transparent hover:before:bg-indigo-500 px-4 py-3 gap-2  "
       rel="noreferrer"
       target={action === "Open" ? "_blank" : "_self"}
     >
-      <div className="flex items-center justify-between leading-6">
-        <div className="flex w-0 flex-1 items-center">
-          <div className="flex min-w-0 flex-1 gap-4 items-center justify-between">
-            <span className="truncate font-medium dark:text-gray-200">
-              {title}
+      <div className="flex justify-between leading-6">
+        <div className="flex w-0 flex-1 flex-col @md:flex-row items-start @md:justify-between flex-wrap @lg:flex-nowrap gap-x-2">
+          <span className="font-medium dark:text-gray-200 leading-5 text-pretty">
+            {title}
+          </span>
+          {caption ? (
+            <span className="mt-0.5 shrink-0 text-gray-400 dark:text-gray-500 text-xs font-sans border px-2 rounded-full border-gray-200 dark:border-gray-700">
+              {caption}
             </span>
-            {caption ? (
-              <span className="shrink-0 text-gray-400 dark:text-gray-500 text-xs font-sans border px-2 rounded-full border-gray-200 dark:border-gray-700">
-                {caption}
-              </span>
-            ) : null}
-          </div>
+          ) : null}
         </div>
         <div className="ml-4 shrink-0">
-          <span className="font-medium text-indigo-600 group-hover:text-indigo-700 dark:text-indigo-400 dark:group-hover:text-indigo-300 flex gap-2 items-center">
-            <span className="max-sm:hidden text-xs">{action}</span>
-            {action === "Open" ? <ExternalLink size={16} /> : <span>❯</span>}
+          <span className="mt-1 font-medium text-indigo-600 group-hover:text-indigo-700 dark:text-indigo-400 dark:group-hover:text-indigo-300 flex gap-2 items-center">
+            <span className="@max-lg:hidden text-xs">{action}</span>
+            {action === "Open" ? (
+              <ExternalLink size={16} />
+            ) : (
+              <span className="leading-none -mt-px">❯</span>
+            )}
           </span>
         </div>
       </div>
